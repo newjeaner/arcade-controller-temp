0385ac1e8d9d4a8a0a47c635bcbf60495a9b9d4a
hae <148511924+newjeaner@users.noreply.github.com>
2023-10-21 12:17:40 -0500

    finished main.cpp (v1)

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       tabsrod                                                   */
-/*    Created:      10/21/2023, 10:09:41 AM                                    */
-/*    Description:  V5 project                                                */
+/*    Author:       newjeaner                                                 */
+/*    Created:      10/21/2023, 10:09:41 AM                                   */
+/*    Description:  tank example                                              */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 #include "vex.h"
@@ -11,18 +11,34 @@
 using namespace vex;
 
 // A global instance of vex::brain used for printing to the V5 brain screen
-vex::brain       Brain;
+vex::brain Brain;
 
 // define your global instances of motors and other devices here
 
+controller controller1 = controller(controllerType::primary); // adjust whether it's primary or secondary
 
-int main() {
+// replace the numbers after 'PORT' with the actual port number (no spaces)
+motor leftFrontMotor = motor(PORT1);
+motor leftMiddleMotor = motor(PORT2);
+motor leftBackMotor = motor(PORT3);
+motor rightFrontMotor = motor(PORT4);
+motor rightMiddleMotor = motor(PORT5);
+motor rightBackMotor = motor(PORT6);
 
-    Brain.Screen.printAt( 10, 50, "Hello V5" );
-   
-    while(1) {
-        
-        // Allow other tasks to run
-        this_thread::sleep_for(10);
+motor_group leftSide = motor_group(leftFrontMotor, leftMiddleMotor, leftBackMotor);
+motor_group rightSide = motor_group(rightFrontMotor, rightMiddleMotor, rightBackMotor);
+
+int main()
+{
+
+    Brain.Screen.printAt(10, 50, "Hello V5");
+
+    while (1)
+    {
+
+        // x & y axis (left joystick)
+        leftSide.spin(directionType::fwd, controller1.Axis3.position() + controller1.Axis1.position(), percentUnits::pct);
+        // x & y axis (right joystick)
+        rightSide.spin(directionType::fwd, controller1.Axis3.position() - controller1.Axis1.position(), percentUnits::pct);
     }
 }
